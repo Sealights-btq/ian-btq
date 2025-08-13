fce85a3d373f8fc738b7f27558c6b09cd45d9829
imfaus <i.michael-flanagan@tricentis.com>
2025-08-13 10:27:02 -0500

    Update node.groovy

--- jenkins/jenkinsfiles/node.groovy
+++ jenkins/jenkinsfiles/node.groovy
@@ -1,7 +1,8 @@
 pipeline {
   agent {
     kubernetes {
-      yaml readTrusted('jenkins/pod-templates/cypress.yaml')
+      yaml readTrusted('jenkins/pod-templates/node.yaml
+')
       defaultContainer "shell"
     }
   }
