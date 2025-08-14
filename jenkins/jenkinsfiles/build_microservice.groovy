69590f99eaf55d08f0d32b48d0d59c5cf6404056
imfaus <i.michael-flanagan@tricentis.com>
2025-08-14 07:11:58 -0500

    Update build_microservice.groovy

--- jenkins/jenkinsfiles/build_microservice.groovy
+++ jenkins/jenkinsfiles/build_microservice.groovy
@@ -1,4 +1,4 @@
-pipeline{
+pipeline {
   agent {
     kubernetes {
       yaml readTrusted('jenkins/pod-templates/BTQ_BUILD_shell_kaniko_pod.yaml')
@@ -9,19 +9,22 @@ pipeline{
   options {
     buildDiscarder logRotator(numToKeepStr: '100')
   }
+
   parameters {
     string(name: 'TAG', defaultValue: '1.2.2', description: 'latest tag')
     string(name: 'BRANCH', defaultValue: 'main', description: 'default branch')
     choice(name: 'SERVICE', choices: ["adservice","cartservice","checkoutservice", "currencyservice","emailservice","frontend","paymentservice","productcatalogservice","recommendationservice","shippingservice","sealightsservice"], description: 'Service name to build')
     string(name: 'BUILD_NAME', defaultValue: 'none', description: 'build name')
   }
-  environment{
+
+  environment {
     ECR_FULL_NAME = "btq-${params.SERVICE}"
     ECR_URI = "474620256508.dkr.ecr.eu-west-1.amazonaws.com/${env.ECR_FULL_NAME}"
     TAG = "ian_${params.TAG}"
+    GITHUB_REPO_URL = "https://github.com/Sealights-btq/ian-btq" // derived from git URL without .git
   }
 
-  stages{
+  stages {
     stage('Init') {
       steps {
         script {
@@ -32,8 +35,9 @@ pipeline{
             // Clone the repository with the specified branch.
             git branch: params.BRANCH, url: 'https://github.com/Sealights-btq/ian-btq.git'
             currentBuild.displayName = "${SERVICE}-${BUILD_NAME}"
+
             stage("Build Docker ${params.SERVICE} Image") {
-              container(name: 'kaniko'){
+              container(name: 'kaniko') {
                 script {
                   def CONTEXT = params.SERVICE == "cartservice" ? "./src/${params.SERVICE}/src" : "./src/${params.SERVICE}"
                   def DP = "${CONTEXT}/Dockerfile"
@@ -41,8 +45,6 @@ pipeline{
                   def BRANCH = params.BRANCH
                   def BUILD_NAME = "${params.BUILD_NAME}"
                   def SL_TOKEN = env.SL_TOKEN
-                  def AGENT_URL = params.AGENT_URL
-                  def AGENT_URL_SLCI = params.AGENT_URL_SLCI
 
                   sh """
                       /kaniko/executor \
@@ -51,7 +53,9 @@ pipeline{
                       --destination ${D} \
                       --build-arg BRANCH=${BRANCH} \
                       --build-arg BUILD_NAME=${BUILD_NAME} \
-                      --build-arg SEALIGHTS_TOKEN=${SL_TOKEN}
+                      --build-arg SEALIGHTS_TOKEN=${SL_TOKEN} \
+                      --build-arg SL_SCM_PROVIDER=github \
+                      --build-arg SL_SCM_BASEURL=${GITHUB_REPO_URL}
                   """
                 }
               }
