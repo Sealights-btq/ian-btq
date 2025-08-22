a07c8d2b7b2188592128e2a15a75b406362092f6
imfaus <i.michael-flanagan@tricentis.com>
2025-08-22 09:08:39 -0500

    Update test_runner.groovy

--- jenkins/jenkinsfiles/test_runner.groovy
+++ jenkins/jenkinsfiles/test_runner.groovy
@@ -94,7 +94,7 @@ pipeline {
                         npm install --save-dev sealights-playwright-plugin
                         echo 'Running Playwright tests with Sealights reporter...'
                         npx playwright test
-                        sleep ${env.wait_time}
+                        
                     """
                 }
             }
