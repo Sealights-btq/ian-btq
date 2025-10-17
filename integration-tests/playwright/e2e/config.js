667c9e58db0f39e9d40f5e8497cc6b1fea47ba4e
bblakereynolds <6922489+bblakereynolds@users.noreply.github.com>
2025-10-17 12:20:02 -0500

    Update config.js

--- integration-tests/playwright/e2e/config.js
+++ integration-tests/playwright/e2e/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    baseUrl: 'https://10.2.10.163:8081',
-    baseUrlNew: 'https://10.2.10.163:8081',
+    baseUrl: 'https://ian-btq.btq.sealights.co/',
+    baseUrlNew: 'https://ian-btq.btq.sealights.co/',
     timeout: 1000,
     username: 'ian@testim.io',
     password: 'Testim123',
